653f5f27714b3c62fe0a7d1106a32db5e1256d9d
Ryan Cheung <ryanzjlib@gmail.com>
2015-02-10 19:18:23 +0800

    exercise done update code

--- python_codes/05.py
+++ python_codes/05.py
@@ -1,6 +1,7 @@
 import sympy
 from sympy.abc import t
 from sympy import Function
+import matplotlib.pyplot as plt
 y = Function('y')
 formula = y(t)**2-t
 
@@ -11,7 +12,7 @@ def eulerMethod(formula,tval,yval,step,numSteps,percision = 5):
         m = round(formula.subs({y(t):yvals[-1],t:tvals[-1]}),percision)
         tvals.append(round(tvals[-1]+step,percision))
         yvals.append(round(yvals[-1]+m*step,percision))
-        print float(i)/numSteps*100
+        # print float(i)/numSteps*100
     return tvals, yvals
 
 
@@ -57,8 +58,96 @@ def plotEulerAndSF(formula,tval,yval,step, tdomain, ydomain,percision=5):
     plt.xlim([tdomain[0],tdomain[-1]])
     plt.ylim([ydomain[0],ydomain[-1]])
 
-
     return fig
 
 fg = plotEulerAndSF(formula, tval, yval, 0.05, tdomain, ydomain)
 fg.show()
+
+
+# ps 1
+formula = (3-y(t))*(y(t)+1)
+tv,yv = eulerMethod(formula,0, 4, 1.0, 10 ,5)
+for i in range(len(tv)):
+    print "after step {0} the t value is {1} and y value is {2}".format(str(i),str(tv[i]),str(yv[i]))
+
+# ps 2
+formula = sympy.E**(2.0/y(t))
+tv,yv = eulerMethod(formula,0, 2, 0.5, 5 ,3)
+for i in range(len(tv)):
+    print "after step {0} the t value is {1} and y value is {2}".format(str(i),str(tv[i]),str(yv[i]))
+
+# ps 3
+formula = y(t)**2-y(t)**3
+tv,yv = eulerMethod(formula,0, 0.2, 0.1, 100 ,3)
+for i in range(len(tv)):
+    print "after step {0} the t value is {1} and y value is {2}".format(str(i),str(tv[i]),str(yv[i]))
+
+# ps 4
+formula = 2*y(t)**3+t**2
+tv,yv = eulerMethod(formula,0, -0.5, 0.1, 20 ,3)
+yv[-1]
+
+def eulerMethodNeg(formula,tval,yval,step,numSteps,percision = 5):
+    tvals = [tval]
+    yvals = [yval]
+    for i in range(numSteps):
+        m = round(formula.subs({y(t):yvals[-1],t:tvals[-1]}),percision)
+        tvals.append(round(tvals[-1]-step,percision))
+        yvals.append(round(yvals[-1]-m*step,percision))
+        # print float(i)/numSteps*100
+    return tvals, yvals
+
+tv,yv = eulerMethodNeg(formula,0, -0.5, 0.1, 20 ,3)
+yv[-1]
+
+# ps 5
+
+formula = y(t)**0.5
+steps = [1.0, 0.5, 0.25]
+for step in steps:
+    tv,yv = eulerMethod(formula,0, 1, step, int(4.0/step) ,3)
+    print yv[-1]
+
+tdomain = np.linspace(0,20,30)
+ydomain = np.linspace(0,100,30)
+
+fg = plotEulerAndSF(formula, 0, 1, 0.01, tdomain, ydomain)
+fg.show()
+
+# ps 6
+formula = 2 - y(t)
+steps = [1.0, 0.5, 0.25]
+for step in steps:
+    tv,yv = eulerMethod(formula,0, 1, step, int(4.0/step) ,3)
+    print yv[-1]
+
+tdomain = np.linspace(0,20,30)
+ydomain = np.linspace(0,4,30)
+
+fg = plotEulerAndSF(formula, 0, 1, 0.01, tdomain, ydomain)
+fg.show()
+
+# ps 7
+
+formula = (3 - y(t))*(y(t) + 1)
+tv,yv = eulerMethod(formula,0, 0, 0.5, 10 ,2)
+
+print yv[-1]
+
+# ps 8
+
+tdomain = np.linspace(0,5,30)
+ydomain = np.linspace(0,5,30)
+fg = plotEulerAndSF(formula, 0, 0, 0.5, tdomain, ydomain)
+fg.show()
+
+# ps 9
+
+formula = 9.8 - 0.18/54.0*y(t)**2
+from sympy import solve
+vmax = solve(formula,y(t))[1]
+tv,yv = eulerMethod(formula,0, 0, 0.01, 1500 ,3)
+for i in range(len(yv)):
+    if yv[i] >= 0.95*vmax:
+        print 0+i*0.01
+        break
