78e4dd79a3c2bc1a0b24c0c148d60a52f0a1782c
Ryan Cheung <ryanzjlib@gmail.com>
2015-02-20 17:47:44 +0800

    update plots and codes

--- python_codes/07.py
+++ python_codes/07.py
@@ -68,6 +68,9 @@ def phaseLine(formula, solutions = None):
     return fig
 
 
+
+
+
 formula = y(t)**2*sympy.cos(y(t))
 fg4 = phaseLine(formula)
 fg4.show()
@@ -79,3 +82,54 @@ fg5.show()
 # ps 2
 formula = (1-y(t))*sympy.sin(y(t))
 phaseLine(formula, [-2.0*pi,-1.0*pi,0, 1.0, pi,2.0*pi])
+
+
+def phaseLine(formula, domain = np.linspace(-1,1), domainFix = False):
+    solutions = solve(Eq(formula,0),y(t))
+    try:
+        solutions.sort()
+        ran = solutions[-1]-solutions[0]
+        if len(solutions) == 1:
+            intervals = [solutions[0]-0.5, solutions[0], solutions[0]+0.5]
+        else:
+            intervals = [float(solutions[0]-0.25*ran)]+solutions+[float(solutions[-1]+0.25*ran)]
+
+        if domainFix:
+            ydomain = domain
+        elif len(solutions) > 1:
+            ydomain = np.linspace(float(solutions[0]-0.25*ran), float(solutions[-1]+0.25*ran))
+        elif len(solutions) == 1:
+            ydomain = np.linspace(float(solutions[0]-1),float(solutions[0]+1))
+    except:
+        if domainFix:
+            ydomain = domain
+        else:
+            ydomain = np.linspace(-1,1)
+
+        if 0 in solutions:
+            signVals = [ydomain[12],ydomain[38]]
+        else:
+            signVals = [0]
+        solutions = []
+
+    fig = plt.figure(num=1, figsize=(1,4))
+    plt.plot([0 for dummy in ydomain],ydomain, color = 'black')
+    plt.plot([0 for dummy in solutions], solutions, 'or')
+    plt.axis(xmin = -0.5, xmax = 0.5)
+
+    if solutions != []:
+        for i in range(len(intervals)-1):
+            midpoint = (intervals[i]+intervals[i+1])/2.0
+            intervalSign = sign(formula.evalf(subs={'y(t)':midpoint}))
+            if intervalSign == -1:
+                plt.text(0, midpoint, u'\u2193',fontname='STIXGeneral', color = 'blue', size=30, va='center', ha='center', clip_on=True)
+            elif intervalSign == 1:
+                plt.text(0, midpoint, u'\u2191',fontname='STIXGeneral', color = 'blue', size=30, va='center', ha='center', clip_on=True)
+    else:
+        for val in signVals:
+            intervalSign = sign(formula.evalf(subs={'y(t)':val}))
+            if intervalSign == -1:
+                plt.text(0, val, u'\u2193',fontname='STIXGeneral', color = 'blue', size=30, va='center', ha='center', clip_on=True)
+            elif intervalSign == 1:
+                plt.text(0, val, u'\u2191',fontname='STIXGeneral', color = 'blue', size=30, va='center', ha='center', clip_on=True)
+    return fig
